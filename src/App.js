fbc317de510c9779a37c648bb4ac5e0ab9ef346f
Alex Karavil <alex@karavil.io>
2020-02-28 11:59:30 -0500

    Set up project structure with components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./components/Header.js";
-
+import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList.js";
+import SearchForm from "./components/SearchForm.js";
 
 export default function App() {
-  return (
-    <main>
-      <Header />
-    </main>
-  );
+   const [searchFilter, setSearchFilter] = useState("");
+   return (
+      <main>
+         <Header />
+         <WelcomePage />
+         <SearchForm />
+         <CharacterList filterBy={searchFilter} />
+      </main>
+   );
 }
