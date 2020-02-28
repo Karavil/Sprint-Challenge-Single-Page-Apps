e4a83715c1b681f480c5290ac257eaedd172de72
Alex Karavil <alex@karavil.io>
2020-02-28 12:24:16 -0500

    API went down, started using dummy data. Card creation is successful

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,9 +3,10 @@ import React from "react";
 export default function CharacterCard({ character }) {
    return (
       <div className="card">
-         <h1>{character.name}</h1>
-         <h2>{character.species}</h2>
-         <h2>{character.origin.name}</h2>
+         <h1>Name: {character.name}</h1>
+         <h2>Species: {character.species}</h2>
+         <h2>Home Planet: {character.origin.name}</h2>
+         <h3>Status: {character.status}</h3>
       </div>
    );
 }
